97042ba4f414193e709b224eed4b4f4abe9dd47a
Feroz Khan <khan.feroz8899@gmail.com>
2025-05-21 23:04:14 +0500

    database issue in unit testing is fixed.

--- ProductManagementSystem/src/test/java/com/ferozkhandev/PizzaOrderingSystem/ProductManagementSystem/ProductManagementSystemApplicationTests.java
+++ ProductManagementSystem/src/test/java/com/ferozkhandev/PizzaOrderingSystem/ProductManagementSystem/ProductManagementSystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.ferozkhandev.PizzaOrderingSystem.ProductManagementSystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductManagementSystemApplicationTests {
 
 	@Test
